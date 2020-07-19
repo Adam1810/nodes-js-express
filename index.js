35a7bf6ec4b75393b405fdca2de795f2c9ee9dd5
Adam1810 <dareal1810@googlemail.com>
2020-07-19 15:35:08 +0200

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const members = require('./Members');
+const logger = require('./middleware/logger')
+const app = express();
+
+
+
+app.use(logger);
+
+  // get all members
+app.get('/api/members', (req, res)=> {
+
+    res.json(members);
+
+});
+
+// set static folder
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
