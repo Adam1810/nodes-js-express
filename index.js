4ebfcde9aee4a46585f3c673a6ba110eab0a7091
Adam1810 <dareal1810@googlemail.com>
2020-07-19 21:34:48 +0200

    end

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
 const members = require('./Members');
-const logger = require('./middleware/logger')
+
 const app = express();
 
+// Init middleware
+// app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-app.use(logger);
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-  // get all members
-app.get('/api/members', (req, res)=> {
+// Homepage Route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'Member App',
+    members
+  })
+);
 
-    res.json(members);
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-});
-
-// set static folder
-
-app.use(express.static(path.join(__dirname, 'public')))
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
